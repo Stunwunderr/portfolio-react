5caaf7db747a8ae0b8dbc20157f557f5ab0011e8
Stunwunderr <90192072+Stunwunderr@users.noreply.github.com>
2023-08-09 03:19:51 +0100

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -169,7 +169,7 @@ export const Container = styled.header`
       top: 0;
       left: 0;
       transition: opacity 0.25s;
-      background-color: white;
+      background-color: #21212150;
 
       a.button{
         background-color: var(--green);
