f25491efc8b7d25cf6c054694c10a61009a0965c
Stunwunderr <90192072+Stunwunderr@users.noreply.github.com>
2023-08-09 21:26:31 +0100

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -17,14 +17,14 @@ export function Portfolio(){
           <header>
           <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
           <div className="project-links">
-            <a href="https://github.com/joaotuliojt/fc-squad43" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
+            <a href="https://dreamweaversrealty.vercel.app" target="_blank" rel="noreferrer">
+              <img src={externalLinkIcon} alt="External Link" />
             </a>
           </div>
           </header>
           <div className="body">
-            <h3>FCallendar</h3>
-            <p>Uma aplicação Web realizada durante o Hackathon do programa de formação da <a href="https://www.fcamara.com.br/"> FCamara</a>. Esta aplicação serve para ajudar na volta do trabalho remoto para o presencial.
+            <h3>Dream Weavers Realty</h3>
+            <p>Discover the essence of real estate excellence with Dream Weavers Realty – your ultimate destination for exceptional properties and personalized service.
             </p>
           </div>
           <footer>
