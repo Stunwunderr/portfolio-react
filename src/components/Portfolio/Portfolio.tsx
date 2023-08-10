beec09d7de1ec9136c4e242ed8fd1dc5c95aedf3
Stunwunderr <90192072+Stunwunderr@users.noreply.github.com>
2023-08-10 08:59:30 +0100

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -43,16 +43,16 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/joaotuliojt/github-search" target="_blank" rel="noreferrer">
+              <a href="https://github.com/CodeWunder/Planets-fact" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="GitHub" />
               </a>
-              <a href="https://github-search-blond.vercel.app/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
+              <a href="https://codewunder-planet.netlify.app/" target="_blank" rel="noreferrer">
+                <img src={externalLinkIcon} alt="Visit site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Github Searcher</h3>
+              <h3>Planets Fact Site</h3>
               <p>Uma versão mais agradável para ver seu perfil do GitHub, inclui visualização de dados , repositórios, compartilhamento entre outros.
               </p>
             </div>
