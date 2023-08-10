6285c5f1c6ffbcb73a12e42134051be511c7ba83
Stunwunderr <90192072+Stunwunderr@users.noreply.github.com>
2023-08-10 10:27:21 +0100

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -146,11 +146,11 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/joaotuliojt/dv-musica-library-page" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="Visitar site" />
+              <a href="https://github.com/codewunder/password-generator" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="Github link" />
               </a>
-              <a href="https://joaotuliojt.github.io/dv-musica-library-page/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
+              <a href="https://codewunder-password-gen.netlify.app/" target="_blank" rel="noreferrer">
+                <img src={externalLinkIcon} alt="Visit site" />
               </a>
             </div>
             </header>
